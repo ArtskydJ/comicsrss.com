238013c18b014e0f6340d0a2b99281af58a712bc
ArtskydJ <josephdykstra@gmail.com>
2017-05-17 02:17:17 -0400

    Build

--- _generator/generate-and-push.sh
+++ _generator/generate-and-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 cd ~/comicsrss.com/
-git pull --ff-only origin gh-pages -q &> /dev/null
+git pull --ff-only origin gh-pages -q > /dev/null
 node _generator
 git add .
 git commit -q -m "Build"
