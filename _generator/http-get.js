040fdf71f787652bb0d3801df879db72a90588de
Joseph Dykstra <josephdykstra@gmail.com>
2017-06-13 21:17:17 -0500

    Implement stupid rate limiting

--- _generator/http-get.js
+++ _generator/http-get.js
@@ -3,16 +3,18 @@ var concat = require('simple-concat')
 
 module.exports = function httpGet(url) {
 	return new Promise(function (resolve, reject) {
-		http.get(url, function (response) {
-			if (response.statusCode >= 400) {
-				return reject(new Error(response.statusCode + ' error'))
-			} else if (response.statusCode >= 300) {
-				return reject(new Error('comic no longer exists'))
-			}
-			concat(response, function (err, buf) {
-				if (err) return reject(err)
-				resolve(buf.toString())
+		setTimeout(function () { // Rate limiting, haha
+			http.get(url, function (response) {
+				if (response.statusCode >= 400) {
+					return reject(new Error(response.statusCode + ' error'))
+				} else if (response.statusCode >= 300) {
+					return reject(new Error('comic no longer exists'))
+				}
+				concat(response, function (err, buf) {
+					if (err) return reject(err)
+					resolve(buf.toString())
+				})
 			})
-		})
+		}, 800)
 	})
 }
