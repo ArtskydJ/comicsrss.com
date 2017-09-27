6d5fba3794b0cdf58dfd523f089aed79bdf72a64
Joseph Dykstra <josephdykstra@gmail.com>
2017-09-27 00:28:04 -0500

    Add titles to the rss and feedly links

--- _generator/generate-main-page.js
+++ _generator/generate-main-page.js
@@ -24,10 +24,10 @@ function linkObjectsToHtml(linkObjects) {
 		return `
 		<li data-search="${linkObject.titleAndAuthor.toLowerCase()}">
 			<span class="comic-title">${linkObject.titleAndAuthor}</span>
-			<a href="${linkObject.feedUrl}" onclick="return copy(this, '${linkObject.feedUrl}')" class="icon-link">
+			<a href="${linkObject.feedUrl}" onclick="return copy(this, '${linkObject.feedUrl}')" class="icon-link" title="Copy RSS URL">
 				<img src="./rss.svg" alt="copy rss feed url for ${linkObject.titleAndAuthor}" class="icon rss-icon">
 			</a>
-			<a href="http://cloud.feedly.com/#subscription%2Ffeed%2F${encodeURIComponent(linkObject.feedUrl)}" target="_blank" class="icon-link">
+			<a href="http://cloud.feedly.com/#subscription%2Ffeed%2F${encodeURIComponent(linkObject.feedUrl)}" target="_blank" class="icon-link" title="Open in feedly">
 				<img src="${feedlyIconUrl}" alt="follow ${linkObject.titleAndAuthor} in feedly" class="icon feedly-icon">
 			</a>
 		</li>`
