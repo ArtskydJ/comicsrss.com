1a64d6d8fd1b3b5c1032b6c8e4d415fa50c9145f
Joseph Dykstra <josephdykstra@gmail.com>
2018-05-17 00:51:30 -0500

    Add suggested comics
    Fixes #75
    Might fix #70

--- _generator/generator/generate-main-page-from-comic-objects.js
+++ _generator/generator/generate-main-page-from-comic-objects.js
@@ -6,11 +6,36 @@ var indexTemplateHtml = fs.readFileSync(indexTemplateHtmlPath, 'utf-8')
 
 module.exports = function generateMainPage(comicObjects) {
 	var rssFeedList = comicObjectsToHtml(comicObjects)
+	var suggestedComicObjects = comicObjects.filter(function (comicObject) {
+		return [
+			'calvinandhobbes',
+			'foxtrot',
+			'foxtrotclassics',
+			'peanuts',
+			'pearlsbeforeswine',
+			'dilbert-classics'
+		].indexOf(comicObject.basename) !== -1
+	})
+	var suggestedPages = suggestedComicsToHtml(suggestedComicObjects)
 	var todaysDate = new Date().toDateString()
 	
 	return indexTemplateHtml
+		.replace('<!-- SUGGESTED PAGES -->', suggestedPages)
 		.replace('<!-- RSS FEED LIST -->', rssFeedList)
 		.replace('<!-- DATE GENERATED -->', todaysDate)
+}
+
+function suggestedComicsToHtml(suggestedComicObjects) {
+	return suggestedComicObjects.map(function (comicObject) {
+		return `
+			<a href="./preview/${encodeURI(comicObject.basename)}" class="suggested-item">
+				<img src="${comicObject.headerImageUrl}">
+				<span class="title-and-author">
+				${comicObject.titleAndAuthor.split(' by ').join('<br>by ')}
+				</span>
+			</a>
+			`.trim()
+	}).join('')
 }
 
 function comicObjectsToHtml(comicObjects) {
