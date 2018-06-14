5ebdd1e9196712af1ce26bb7a4d60b3ab2c7e0a1
Joseph Dykstra <josephdykstra@gmail.com>
2018-06-13 23:48:12 -0500

    Input email address
    Fixes #78

--- _install.sh
+++ _install.sh
@@ -1,8 +1,9 @@
 cd /root
 git clone git@github.com:ArtskydJ/comicsrss.com.git || true
 apt-get install sendmail -y
+read -p "Enter your email address: " comicsrss_email_address
 crontab -l > ./crontab.txt
-echo "MAILTO=josephdykstra@gmail.com" >> ./crontab.txt
+echo "MAILTO=$comicsrss_email_address" >> ./crontab.txt
 echo "# Runs at 1:15 CDT. It would work at 12:15, but I don't want to" >> ./crontab.txt
 echo "# have to change it for DST. Not sure if I would have to or not..." >> ./crontab.txt
 echo "15 2 * * * sh /root/comicsrss.com/_generator/generate-and-push.sh" >> ./crontab.txt
