57aaca6dc6e4bd9fae5458af5ea5cbb4a9796c52
Joseph Dykstra <josephdykstra@gmail.com>
2018-08-07 00:33:17 -0500

    Use new links

--- _generator/generator/index.js
+++ _generator/generator/index.js
@@ -4,7 +4,9 @@ var generateRssFeedFromComicObject = require('./generate-rss-feed-from-comic-obj
 var generatePreviewPageFromComicObject = require('./generate-preview-page-from-comic-object.js')
 var generateMainPageFromComicObjects = require('./generate-main-page-from-comic-objects.js')
 var comicObjects = require('../tmp/_comic-objects')
-var rssitemcontentTemplate = fs.readFileSync('./template/rssitemcontent-template.html')
+
+var rssitemcontentTemplateHtmlPath = path.resolve(__dirname, 'template', 'rssitemcontent-template.html')
+var rssitemcontentTemplate = fs.readFileSync(rssitemcontentTemplateHtmlPath, 'utf-8')
 
 function writeFilesFromComicObjects(comicObjects) {
 	comicObjects = comicObjects.filter(Boolean)
@@ -23,7 +25,8 @@ function writeFilesFromComicObjects(comicObjects) {
 		comicObject.comicStrips.slice(0, 5).forEach(function (comicStrip) {
 			var uniqueString = comicStrip.comicImageUrl.split('/').pop()
 			var filename = '../../rssitemcontent/' + uniqueString.slice(0, 3) + '/' + uniqueString + '.html'
-			var rssitemcontent = rssitemcontentTemplate.replace(/<!-- COMIC IMAGE URL -->/, comicStrip.comicImageUrl)
+			var rssitemcontent = rssitemcontentTemplate
+				.replace(/<!-- COMIC IMAGE URL -->/, comicStrip.comicImageUrl)
 			writeFile(filename, rssitemcontent)
 		})
 	})
