1349804edcd74d4fab668678ff6db6283738acd9
Joseph Dykstra <josephdykstra@gmail.com>
2018-11-15 08:31:57 -0600

    Change supporters to use mustache templating
    This actually caused a net increase of LOC. However, the logic is now less
    confusing for what each level has access to. I should be able to simplify the
    supporters.json if I wanted to, now.
    
    Next to change templating:
    preview-template.html
    and make a standard template that has the html head, body nav, and body footer
    then put supporters, preview, and index into that.

--- _generator/generator/generate-supporters-page-from-json.js
+++ _generator/generator/generate-supporters-page-from-json.js
@@ -1,35 +1,13 @@
-var fs = require('fs')
+// var fs = require('fs')
 var path = require('path')
+var mustache = require('art-template')
 
 var supportersTemplateHtmlPath = path.resolve(__dirname, 'template', 'supporters-template.html')
-var supportersTemplateHtml = fs.readFileSync(supportersTemplateHtmlPath, 'utf-8')
+// var supportersTemplateHtml = fs.readFileSync(supportersTemplateHtmlPath, 'utf-8')
 
 module.exports = function generateSupportersPage(supporters) {
-	var supportersHtml = `
-	<div class="supporters">
-		<div class="gold-supporters">${generateSupportersHtml(supporters.gold, 'Gold', '2790040')}</div>
-		<div class="silver-supporters">${generateSupportersHtml(supporters.silver, 'Silver', '2790035')}</div>
-		<div class="bronze-supporters">${generateSupportersHtml(supporters.bronze, 'Bronze', '2275262')}</div>
-	</div>
-	`.trim()
-	var todaysDate = new Date().toDateString()
-	return supportersTemplateHtml
-		.replace('<!-- SUPPORTERS -->', supportersHtml)
-		.replace('<!-- DATE GENERATED -->', todaysDate)
-}
-
-function generateSupportersHtml(supporters, level, rid) {
-	return	`<h2>${level} Supporters</h2>` +
-	supporters.map(function (supporter) {
-		return '<div class="supporter">'+
-			(supporter.url ? `<a href="${supporter.url}">` : '') +
-			`<span class="supporter-name">${supporter.displayName}</span>` +
-			(supporter.url ? `</a>` : '') +
-			(supporter.description ? `<span class="supporter-name">${supporter.description}</span>` : '') +
-			'</div>'
-		.trim()
-	}) + 
-	`<a href="https://www.patreon.com/join/josephdykstra/checkout?rid=${rid}">
-		${supporters.length ? 'Also become a '+level+' supporter' : 'Be the first '+level+' supporter'}
-	</a>`
+	return mustache(supportersTemplateHtmlPath, {
+		dateGenerated: new Date().toDateString(),
+		supporters: supporters
+	})
 }
