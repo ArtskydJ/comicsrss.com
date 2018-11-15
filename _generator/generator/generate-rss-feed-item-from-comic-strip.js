3a198f711613cb5c6716d8be1a02877993e605e8
Joseph Dykstra <josephdykstra@gmail.com>
2018-11-15 07:49:19 -0600

    Change RSS Feed Item generator to use mustache

--- _generator/generator/generate-rss-feed-item-from-comic-strip.js
+++ _generator/generator/generate-rss-feed-item-from-comic-strip.js
@@ -1,14 +1,14 @@
 var fs = require('fs')
 var path = require('path')
+var mustache = require('art-template')
 
 var rssitemcontentTemplateHtmlPath = path.resolve(__dirname, 'template', 'rssitemcontent-template.html')
 var rssitemcontentTemplate = fs.readFileSync(rssitemcontentTemplateHtmlPath, 'utf-8')
 
 module.exports = function generateHtml(comicObject, comicStrip) {
-	return rssitemcontentTemplate
-		.replace(/<!-- GOCOMICS URL -->/g, comicStrip.url)
-		.replace(/<!-- COMICSRSS URL -->/g, 'https://www.comicsrss.com/preview/' + encodeURI(comicObject.basename))
-		.replace(/<!-- COMIC IMAGE URL -->/g, comicStrip.comicImageUrl)
-		.replace(/<!-- TITLE AUTHOR DATE -->/g, comicStrip.titleAuthorDate)
+	return mustache.render(rssitemcontentTemplate, {
+		comicsRssUrl: 'https://www.comicsrss.com/preview/' + encodeURI(comicObject.basename),
+		comicStrip: comicStrip
+	})
 }
 
