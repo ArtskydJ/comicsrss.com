301b9c3e6ba32519c255d8467fe911ab921c17c9
Joseph Dykstra <josephdykstra@gmail.com>
2018-11-14 23:21:14 -0600

    Use a template engine for the main page

--- _generator/generator/generate-main-page-from-comic-objects.js
+++ _generator/generator/generate-main-page-from-comic-objects.js
@@ -1,11 +1,11 @@
 var fs = require('fs')
 var path = require('path')
+var mustache = require('art-template')
 
 var indexTemplateHtmlPath = path.resolve(__dirname, 'template', 'index-template.html')
 var indexTemplateHtml = fs.readFileSync(indexTemplateHtmlPath, 'utf-8')
 
 module.exports = function generateMainPage(comicObjects) {
-	var rssFeedList = comicObjectsToHtml(comicObjects)
 	var suggestedComicObjects = comicObjects.filter(function (comicObject) {
 		return [
 			'calvinandhobbes',
@@ -15,41 +15,20 @@ module.exports = function generateMainPage(comicObjects) {
 			'pearlsbeforeswine',
 			'dilbert-classics'
 		].indexOf(comicObject.basename) !== -1
-	})
-	var suggestedPages = suggestedComicsToHtml(suggestedComicObjects)
-	var todaysDate = new Date().toDateString()
+	}).sort(comicObjectSorter)
+
+	var sortedComicObjects = comicObjects.sort(comicObjectSorter)
 	
-	return indexTemplateHtml
-		.replace('<!-- SUGGESTED PAGES -->', suggestedPages)
-		.replace('<!-- RSS FEED LIST -->', rssFeedList)
-		.replace('<!-- DATE GENERATED -->', todaysDate)
+	return mustache.render(indexTemplateHtml, {
+		suggestedComicObjects: suggestedComicObjects,
+		comicObjects: sortedComicObjects,
+		generatedDate: new Date().toDateString(),
+		encodeURI: encodeURI
+	})
 }
 
-function suggestedComicsToHtml(suggestedComicObjects) {
-	return suggestedComicObjects.map(function (comicObject) {
-		return `
-			<a href="./preview/${encodeURI(comicObject.basename)}" class="suggested-item">
-				<img src="./static/suggested/resized/${comicObject.basename}.jpg">
-				<span class="title-and-author">
-					<div class="title">${comicObject.titleAndAuthor.split(' by ')[0]}</div>
-					<div class="author">by ${comicObject.titleAndAuthor.split(' by ')[1]}</div>
-				</span>
-			</a>
-			`.trim()
-	}).join('')
-}
-
-function comicObjectsToHtml(comicObjects) {
-	return '<ul id="comics-list">' +
-	comicObjects
-		.filter(Boolean)
-		.map(function (comicObject) {
-			return `
-				<li data-search="${comicObject.titleAndAuthor.toLowerCase()}">
-					<a href="./preview/${encodeURI(comicObject.basename)}" class="comic-title">${comicObject.titleAndAuthor}</a>
-				</li>`.replace(/^\t{4}/mg, '')
-		})
-		.sort()
-		.join('\n') +
-	'</ul>'
+function comicObjectSorter(aa, bb) {
+	var a = aa.titleAndAuthor.toLowerCase()
+	var b = bb.titleAndAuthor.toLowerCase()
+	return a > b ? 1 : (b > a ? -1 : 0)
 }
