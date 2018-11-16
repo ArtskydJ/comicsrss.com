8f965c27c651bf3c5641b54a8651fb80fb59f670
Joseph Dykstra <josephdykstra@gmail.com>
2018-11-15 23:08:02 -0600

    Make preview pages use templating

--- _generator/generator/generate-preview-page-from-comic-object.js
+++ _generator/generator/generate-preview-page-from-comic-object.js
@@ -1,43 +1,21 @@
 var fs = require('fs')
 var path = require('path')
+var mustache = require('art-template')
 
 var previewTemplateHtmlPath = path.resolve(__dirname, 'template', 'preview-template.html')
 var previewTemplateHtml = fs.readFileSync(previewTemplateHtmlPath, 'utf-8')
 
 module.exports = function generatePreviewPage(comicObject) {
-// 	var rssFeedList = comicObjectsToHtml(comicObjects)
-	var comicsRssPreviewUrl = 'https://www.comicsrss.com/preview/' + encodeURI(comicObject.basename)
 	var comicsRssFeedUrl = 'https://www.comicsrss.com/rss/' + encodeURI(comicObject.basename) + '.rss'
-	var feedlyFeedUrl = 'https://feedly.com/i/subscription/feed/' + encodeURIComponent(comicsRssFeedUrl)
-	var comicImagesHtml = generateComicImagesHtml(comicObject)
-	var todaysDate = new Date().toDateString()
 
-	return previewTemplateHtml
-		.replace('<!-- COMIC TITLE -->', comicObject.titleAndAuthor)
-		.replace('<!-- GOCOMICS URL -->', comicObject.comicUrl)
-		.replace('<!-- FEEDLY FEED URL -->', feedlyFeedUrl)
-		.replace('<!-- RSS URL -->', comicsRssFeedUrl)
-		.replace('<!-- COMIC IMAGES -->', comicImagesHtml)
-		.replace('<!-- DATE GENERATED -->', todaysDate)
-}
-
-function generateComicImagesHtml(comicObject) {
-	return comicObject.comicStrips
-	.slice(0, 14) // two weeks
-	.map(function (comicStrip) {
-		var comicStripDate = new Date(comicStrip.titleAuthorDate.split(' for ').pop()).toISOString().slice(0, 10)
-		return `<p id="${comicStripDate}">
-			<div>
-				<span class="preview-header">${comicStrip.titleAuthorDate}</span>
-				<a href="#${comicStripDate}">
-					<img src="../static/link_sm.png" alt="Link to this comic strip">
-				</a>
-				<a href="${comicStrip.url}">
-					<img src="../static/gocomics_sm.png" alt="View on GoComics">
-				</a>
-			</div>
-			<img class="preview-comic" src="${comicStrip.comicImageUrl}">
-		</p>`
+	return mustache.render(previewTemplateHtml, {
+		comicObject: comicObject,
+		dateGenerated: new Date().toDateString(),
+		comicsRssPreviewUrl: 'https://www.comicsrss.com/preview/' + encodeURI(comicObject.basename),
+		comicsRssFeedUrl: comicsRssFeedUrl,
+		feedlyFeedUrl: 'https://feedly.com/i/subscription/feed/' + encodeURIComponent(comicsRssFeedUrl),
+		isoDate: function isoDate(date) {
+			return new Date(date).toISOString().slice(0, 10)
+		}
 	})
-	.join('')
 }
