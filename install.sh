ca870143f0b5b1891d8a6afbe73e903906ca50e0
Joseph Dykstra <josephdykstra@gmail.com>
2019-03-18 22:44:10 -0500

    Run hourly

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ apt-get install sendmail -y
 read -p "Enter your email address: " comicsrss_email_address
 crontab -l > ./crontab.txt
 echo "MAILTO=$comicsrss_email_address" >> ./crontab.txt
-echo "# Runs at 1:15 CDT. It would work at 12:15, but I don't want to" >> ./crontab.txt
-echo "# have to change it for DST. Not sure if I would have to or not..." >> ./crontab.txt
-echo "15 2 * * * sh /root/comicsrss.com/_generator/generate-and-push.sh" >> ./crontab.txt
+echo "# Runs hourly at 1:15, 2:15, 3:15, etc." >> ./crontab.txt
+echo "15 * * * * sh /root/comicsrss.com/_generator/generate-and-push.sh" >> ./crontab.txt
 crontab ./crontab.txt
