99d3c1a0bec3b3cb76b25603e4e98e8ce89fb66f
Joseph Dykstra <josephdykstra@gmail.com>
2019-06-25 00:18:23 -0500

    Make the commit quiet to avoid extra emails

--- _generator/generate-and-push.sh
+++ _generator/generate-and-push.sh
@@ -3,6 +3,6 @@ cd ~/comicsrss.com/
 git pull --ff-only origin gh-pages -q > /dev/null
 node _generator/bin scrape generate
 git add .
-git commit -q -m "Build"
+git commit -q -m "Build" > /dev/null
 git gc --auto --quiet
 git push origin gh-pages -q
