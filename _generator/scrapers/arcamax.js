f37646559062a7e27461a7e845b8d408409e07ab
ArtskydJ <josephdykstra@gmail.com>
2021-10-30 13:52:36 -0500

    Use query-html in arcamax

--- _generator/scrapers/arcamax.js
+++ _generator/scrapers/arcamax.js
@@ -1,26 +1,21 @@
-const { decode } = require('querystring')
 const fetch = require('./lib/fetch.js')
 const multipageScraper = require('./lib/multipage-scraper.js')
+const { query_html, element_to_text } = require('./lib/query-html.js')
 
 const unavailableComics = [
 	'thepajamadiaries',
 	'nestheads',
 ]
 
-function between(str, begin, end) {
-	return (str.split(begin, 2)[1] || '').split(end, 1)[0]
-}
-
 async function getSeriesObjects() {
 	const html = await fetch('https://www.arcamax.com/comics')
-	const seriesEntries = between(html, 'Comics A-Z', 'Editorial Cartoons')
-		.split('\n')
-		.filter(l => l.startsWith('<li><a href="/thefunnies/'))
-		.map(line => {
-			const href = between(line, '<li><a href="', '"')
-			const basename = between(line, 'data-code="', '"')
-			const title = between(line, '" >', '</a>')
-			// const author = between(html, '<cite>', '</cite>')
+	const $ = query_html(html)
+
+	const seriesEntries = $('.side-column ul:first-child li a')
+		.map(a_element => {
+			const href = a_element.attribs.href
+			const basename = a_element.attribs['data-code']
+			const title = element_to_text(a_element)
 
 			return [ basename, {
 				// author, // The author info is not in the sidebar
@@ -37,40 +32,40 @@ async function getSeriesObjects() {
 
 async function getStrip(stripPageUrl) {
 	const html = await fetch(stripPageUrl)
-	const urlMatches = html.match(/<a href="https:\/\/www\.facebook\.com\/sharer\.php\?(.+?)" class="facebook" target="_blank">/)
-	if (urlMatches === null || ! urlMatches[1]) throw new Error('Unable to parse url')
-	// u=https%3A%2F%2Fwww.arcamax.com%2Fthefunnies%2Fmutts%2Fs-2375148&amp;h=Mutts+for+6%2F23%2F2020
-	const shareParams = decode(urlMatches[1].replace(/&amp;/g, '&'))
-	const url = shareParams.u
-	const m_d_yyyyDate = shareParams.h.trim().split(' ').pop()
-	const mmm_d_dateMatches = html.match(/<span class="cur">(.+?)<\/span>/)
+	const $ = query_html(html)
+
+	const facebook_url = $('.facebook')[0].href.replace(/&amp;/g, '&')
+	const facebook_url_params = new URL(facebook_url).searchParams
+	// https://www.facebook.com/sharer.php?u=https%3A%2F%2Fwww.arcamax.com%2Fthefunnies%2Fmutts%2Fs-2375148&amp;h=Mutts+for+6%2F23%2F2020
+	const url = facebook_url_params.u
+	const m_d_yyyyDate = facebook_url_params.h.trim().split(' ').pop()
+	const mmm_d_date = element_to_text($('span.cur')[0])
 	const date = m_d_yyyyDate.includes('/')
 		? usDateToIsoDate(m_d_yyyyDate)
-		: mmm_d_dateMatches && humanReadableDateToIsoDate(mmm_d_dateMatches[1])
+		: mmm_d_date && humanReadableDateToIsoDate(mmm_d_date)
 
-	const imageUrlMatches = html.match(/<img id="comic-zoom".+src="([^">]+)"/)
-	const authorMatches = html.match(/<cite>by (.+?)<\/cite>/)
+	const imageUrl = new URL($('img#comic-zoom')[0].attribs.src, 'https://www.arcamax.com').toString()
+	const author = element_to_text($('cite')[0]).replace(/^by /, '')
 	const isOldestStrip = /class="prev-off"/.test(html)
-	const olderRelUrlMatches = html.match(/<a.+class=["']prev["'].+href=["'](.*?)["']/)
-	// const newerRelUrlMatches = html.match(/<a.+class=["']next["'].+href=["'](.*?)["']/)
-	const headerImageUrlMatches = html.match(/<meta property="og:image" content=["'](.+?)["']/)
+	const olderRelUrl = isOldestStrip || $('a.prev')[0].attribs.href
+	// const newerRelUrlMatches = $('a.next')[0].attribs.href
+	const headerImageUrl = $('meta[property="og:image"]')[0].attribs.content
 
 	if (imageUrlMatches === null || ! imageUrlMatches[1]) throw new Error('Unable to parse comicImageUrl in ' + url)
 	if (authorMatches === null || ! authorMatches[1]) throw new Error('Unable to parse author in ' + url)
 	if ((olderRelUrlMatches === null || ! olderRelUrlMatches[1]) && ! isOldestStrip) throw new Error('Unable to parse olderRelUrl in ' + url)
-	if (headerImageUrlMatches === null || ! headerImageUrlMatches[1]) throw new Error('Unable to parse headerImageUrlMatches in ' + url)
 	// if (newerRelUrlMatches === null) throw new Error('Unable to parse newerRelUrl in ' + url)
 
 
 	return {
-		imageUrl: new URL(imageUrlMatches[1], 'https://www.arcamax.com').toString(),
+		imageUrl,
 		date,
-		author: authorMatches[1],
+		author,
 		url,
 		isOldestStrip,
-		olderRelUrl: isOldestStrip || olderRelUrlMatches[1],
+		olderRelUrl,
 		// newerRelUrl: newerRelUrlMatches[1],
-		headerImageUrl: headerImageUrlMatches[1]
+		headerImageUrl,
 	}
 }
 
--- _generator/tmp/revert-all-series-objects.bat
+++ _generator/tmp/revert-all-series-objects.bat
@@ -1,2 +1,3 @@
+git checkout gh-pages _generator/tmp/_arcamax-series-objects.json
 git checkout gh-pages _generator/tmp/_gocomics-series-objects.json
 git checkout gh-pages _generator/tmp/_dilbert-series-objects.json
