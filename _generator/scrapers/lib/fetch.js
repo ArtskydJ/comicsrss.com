b546195dae40831022b2e4f40b309e6209f85abf
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2022-11-22 09:51:28 -0600

    Use node's built-in `fetch` for improved error handling

--- _generator/scrapers/lib/fetch.js
+++ _generator/scrapers/lib/fetch.js
@@ -1,21 +1,4 @@
-const https = require('https')
-
-module.exports = function fetch(url) {
-	return new Promise((resolve, reject) => {
-		if (global.VERBOSE) {
-			console.log('GET ' + url)
-		}
-		https.get(url, res => {
-			if (res.statusCode !== 200) {
-				const err = new Error(res.statusCode + ' error')
-				err.res = res
-				return reject(err)
-			}
-
-			const chunks = []
-			res.on('data', chunk => chunks.push(chunk))
-			res.once('end', () => resolve(Buffer.concat(chunks).toString()))
-			res.once('error', reject)
-		})
-	})
+module.exports = async url => {
+	const res = await fetch(url)
+	return res.text()
 }
