a20326ca1fd4b592b678dfa882a96af6b362de65
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2024-03-26 23:41:15 -0500

    Change line endings
    Also switch spaces to tabs

--- _generator/scrapers/lib/query-html.js
+++ _generator/scrapers/lib/query-html.js
@@ -2,17 +2,17 @@ const cssSelect = require('css-select')
 const { parseDOM } = require('htmlparser2')
 
 function element_to_text(ele) {
-    return ele.children.map(child => child.data || (child.children ? element_to_text(child) : '')).join('')
+	return ele.children.map(child => child.data || (child.children ? element_to_text(child) : '')).join('')
 }
 
 function query_html(html) {
-    const dom = parseDOM(html)
-    return function $(selector, subdom) {
-        return cssSelect.selectAll(selector, subdom || dom)
-    }
+	const dom = parseDOM(html)
+	return function $(selector, subdom) {
+		return cssSelect.selectAll(selector, subdom || dom)
+	}
 }
 
 module.exports = {
-    element_to_text,
-    query_html,
+	element_to_text,
+	query_html,
 }
