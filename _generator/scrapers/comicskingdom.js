029ed231b4993fe54dd436e875d71736c03d3e80
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2024-03-26 23:17:26 -0500

    rewrite comicskingdom scraper

--- _generator/scrapers/comicskingdom.js
+++ _generator/scrapers/comicskingdom.js
@@ -1,54 +1,53 @@
-const fetch = require('./lib/fetch.js')
-const multipageScraper = require('./lib/multipage-scraper.js')
+const fetch2 = require('./lib/fetch.js')
 const { query_html, element_to_text } = require('./lib/query-html.js')
 
-async function getSeriesObjects() {
-	const base = 'https://comicskingdom.com/'
-	const html = await fetch(base)
+module.exports = async function main(cachedSeriesObjects) {
+	const base = 'https://comicskingdom.com/features'
+	const html = await fetch2(base)
 	const $ = query_html(html)
 
-	const seriesObjectEntries = $('.comics-lists .comics-list')
-		.flatMap(list_div_element => {
-			const a_elements = $('a', list_div_element)
-			const is_political = element_to_text($('.comics-list-header', list_div_element)[0]).trim() === 'Political'
-			return a_elements
-				.map(a_element => ({ a_element, is_political }))
-		})
-		.map(({ a_element, is_political }) => {
-			const href = a_element.attribs.href
-			const slug = href.startsWith(base) ? href.replace(base, '') : href
+	const seriesObjectEntries = $('main ul > li > ul > li > a')
+		.map((a_element) => {
+			const parsed = new URL(a_element.attribs.href, base)
+			const slug = parsed.pathname.replace(/^\//, '')
 			const title = element_to_text(a_element)
 
 			return [ slug, {
-				author: null,
+				// author: null,
+				// isPolitical: null,
 				title,
-				url: base + slug,
-				mostRecentStripUrl: base + slug + '/' + (new Date().toISOString().slice(0, 10)),
-				isPolitical: is_political,
-				language: 'eng'
+				url: parsed.href,
+				language: title.endsWith('(Spanish)') ? 'spa' : 'eng',
 			}]
 		})
+		.slice(0, global.DEBUG ? 10 : Infinity)
+	console.log(`found ${ seriesObjectEntries.length } entries`)
+
+	for (const [ slug, seriesObject ] of seriesObjectEntries) {
+		new Promise(resolve => setTimeout(resolve, global.DEBUG ? 0 : 1000)) // rate limit
+
+		console.log(slug)
+		const json = await fetch2(`https://wp.comicskingdom.com/wp-json/wp/v2/ck_comic?ck_feature=${ slug }&before_ymd=${ new Date().toISOString().slice(0, 10) }&per_page=40&date_inclusive=true&order=desc&page=1&_embed=true`)
+		const arr = JSON.parse(json)
+		if (!(Array.isArray(arr) && arr.length)) {
+			throw new Error(`didn't get an array`)
+		}
+
+		seriesObject.isPolitical = arr[0].ck_genre?.name === 'Political'
+
+		const strips = arr.map(e => ({
+			imageUrl: e.assets.single.url,
+			date: e.date.slice(0, 10),
+			url: e.link,
+		}))
+
+		const oldestStripDate = cachedSeriesObjects[slug]?.strips?.[0]?.date || '0000-00-00'
+
+		seriesObject.strips = [
+			...strips.filter(s => s.date > oldestStripDate),
+			...(cachedSeriesObjects[slug]?.strips || []),
+		]
+	}
 
 	return Object.fromEntries(seriesObjectEntries)
 }
-
-async function getStrip(stripPageUrl) {
-	const html = await fetch(stripPageUrl)
-	const $ = query_html(html)
-
-	const permalink = new URLSearchParams($('a[aria-label="FaceBook Share icon"]')[0].attribs.href.split('?')[1]).get('href')
-	const older_a_element = $('.cv-nav:first-child a')[0]
-
-	return {
-		imageUrl: $('img#theComicImage')[0].attribs.src,
-		date: permalink.slice(-10),
-		author: element_to_text($('title')[0]).replace(/^.+? by /, ''),
-		url: permalink,
-		isOldestStrip: !older_a_element,
-		olderRelUrl: older_a_element?.attribs?.href,
-		newerRelUrl: $('.cv-nav:last-child a')[0].attribs.href,
-		headerImageUrl: $('.feature-logo')[0]?.attribs?.src,
-	}
-}
-
-module.exports = cachedSeriesObjects => multipageScraper({ getSeriesObjects, getStrip, cachedSeriesObjects })
