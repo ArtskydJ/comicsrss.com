ca8e33c4916971164966ca3b491693e794ce6aa5
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2024-03-26 23:17:57 -0500

    Improve element_to_text to find text deeply

--- _generator/scrapers/lib/query-html.js
+++ _generator/scrapers/lib/query-html.js
@@ -2,7 +2,7 @@ const cssSelect = require('css-select')
 const { parseDOM } = require('htmlparser2')
 
 function element_to_text(ele) {
-    return ele.children[0].data
+    return ele.children.map(child => child.data || (child.children ? element_to_text(child) : '')).join('')
 }
 
 function query_html(html) {
