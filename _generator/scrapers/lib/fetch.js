3b770f191d5cc0cb1d091a5bb3c4fe66bf4a9544
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2025-04-05 16:32:04 -0500

    improve logging [skip ci]

--- _generator/scrapers/lib/fetch.js
+++ _generator/scrapers/lib/fetch.js
@@ -1,7 +1,10 @@
 module.exports = async url => {
+	if (global.VERBOSE) {
+		console.log('fetching ' + url)
+	}
 	const res = await fetch(url)
 	if (!res.ok) {
-		throw new Error(res.statusText)
+		throw new Error(res.status + ' ' + res.statusText + ' (' + url + ')')
 	}
 	return res.text()
 }
